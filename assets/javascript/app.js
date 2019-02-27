04951b3e0fc6aeff21d894f5e3d80fa551c1aee4
RatofWrath <T_mullen93@yahoo.com>
2019-02-26 22:41:25 -0600

    added try and catch block

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,37 +9,37 @@ var questions = [
    {
     question: "1. Who Broke the record for single season passing yards in 1984?",
     choices: ["Dan Marino", "Jim Kelly", "Joe Montana", "John Elway"],
-    correctAnswer: 0
+    correctAnswer: "A"
    }, 
 
    {
     question: "2. Who currently holds the most career rushing yards in the NFL?",
     choices: ["Barry Sanders", "Emmitt Smith", "Jim Brown", "Walter Payton"],
-    correctAnswer: 1
+    correctAnswer: "B"
    },
    
    {
     question: "3. What team went 17-0 in 1972",
     choices: ["Miami Dolphins", "New York Giants", "Pittsburgh Steelers", "San Francisco 49ers"],
-    correctAnswer: 0
+    correctAnswer: "A"
    },
 
    {
     question: "4. What team won the first Super Bowl",
     choices: ["Cleveland Browns", "Chicago Bears", "Dallas Cowboys", "Green Bay Packers"],
-    correctAnswer: 3
+    correctAnswer: "D"
    },
 
    {
     question: "5. Which head coach has the most Super Bowl victories without a loss?",
     choices: ["Bill Belichick", "Bill Walsh", "Chuck Noll", "Don Shula"],
-    correctAnswer: 2
+    correctAnswer: "C"
    },
 
    {
-    question: "6. Who current hold the record for most reciving touchdowns in a season?",
+    question: "6. Who current holds the record for most reciving touchdowns in a season?",
     choices: ["Calvin Johnson", "Jerry Rice", "Michael Irvin", "Randy Moss"],
-    correctAnswer: 3
+    correctAnswer: "D"
    }
 ];
 
@@ -47,6 +47,7 @@ function startGame(){
     for(i = 0; i < questions.length; i++){
         generateQuestion(i);
     }
+    quizContent += "<button id = 'submit' type = 'button' onclick='displayResults()'> Submit your answers </button>";
     document.getElementById("quizContent").innerHTML = quizContent;
 }
 
@@ -60,24 +61,36 @@ function generateQuestion(i){
 }
 
 function checkAnswers(){
-    for(i = 0; i < questions.length; i++){
-        var radioValue = $("input[name='attempt']:checked").val();
-        if(radioValue){
-            if(radioValue === questions[i].correctAnswer){
-                correctAnswers++;
-                unansweredQuestions--;
-            }
 
-            else{
-                incorrectAnswers++;
-                unansweredQuestions--;
-            }
+    for(i = 0; i < questions.length; i++){
+        try{
+        var attempt = attemptArray[i];
+        var question = questions[i];
+        debugger
+        radioValue = document.querySelector('input[name=' + attempt + ']:checked').value;
+
+        if(radioValue === question.correctAnswer){
+            correctAnswers++;
+            unansweredQuestions--;
         }
-    }
+
+        else{
+            incorrectAnswers++;
+            unansweredQuestions--;
+        }
+
+        }
+
+        catch(error){
+            
+        }
+}
+
 }
 
 function displayResults(){
+    checkAnswers();
     document.getElementById("quizContent").innerHTML = "Correct answers: " + correctAnswers +"<br>"
     + "Incorrect answers: " + incorrectAnswers + "<br>"
-    + "Unanswered questions" + unansweredQuestions;
+    + "Unanswered questions: " + unansweredQuestions;
 }
